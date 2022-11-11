7794048e6db83eeb40b8f13e072f1834d97d8d74
Levine <mugenilevie@gmail.com>
2022-11-11 04:39:27 -0600

    initial commit

---
+++ app.py
@@ -0,0 +1,7 @@
+
+def replace_word():
+    str = "Hurray its friday!"
+    word_to_replace = input("Enter word to replace:")
+    word = input("Enter the replacement: ")
+    print(str.replace(word_to_replace, word)) #allows you to replace a value.
+replace_word()
