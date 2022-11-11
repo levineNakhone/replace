8cafcb918ce00ef1ecb8e61c5c9101ac0d84dc91
Levine <mugenilevie@gmail.com>
2022-11-11 04:43:23 -0600

    commit changes

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 
 def replace_word():
-    str = "Hurray its friday!"
+    str = "I am learning spanish!"
     word_to_replace = input("Enter word to replace:")
     word = input("Enter the replacement: ")
     print(str.replace(word_to_replace, word)) #allows you to replace a value.
 replace_word()
+
